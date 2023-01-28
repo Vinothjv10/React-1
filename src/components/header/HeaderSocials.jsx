7867ab24466a9f84e2ed14ba01d92177548948dd
Vinothjv10 <vinothjv10@gmail.com>
2023-01-28 17:09:40 +0530

    target value changed

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,9 +8,9 @@ import { FaInstagram } from 'react-icons/fa'
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://www.linkedin.com/in/vinothjv/" target="_blank"> <BsLinkedin /> </a>
-            <a href="https://github.com/Vinothjv10" target="_blank"><FaGithub /></a>
-            <a href="https://www.instagram.com/i_m_vinoth__/" target="_blank"><FaInstagram /></a>
+            <a href="https://www.linkedin.com/in/vinothjv/" target="#"> <BsLinkedin /> </a>
+            <a href="https://github.com/Vinothjv10" target="#"><FaGithub /></a>
+            <a href="https://www.instagram.com/i_m_vinoth__/" target="#"><FaInstagram /></a>
         </div>
     )
 }
